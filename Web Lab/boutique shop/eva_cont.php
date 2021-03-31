9e5009cbdd7c1716d708eae6d15dee01eaf2fe84
anjanaviswanath <77048019+anjanaviswanath@users.noreply.github.com>
2021-03-31 13:11:57 +0530

    Web

---
+++ Web Lab/boutique shop/eva_cont.php
@@ -0,0 +1,11 @@
+<html>
+
+<body><center>
+<h3>Address</h3>
+<p>Eva boutique<br> kollam junction<br> phone-0476 25490 <br> mail-eva@gmail.com
+
+<br>
+</p>
+</center>
+</body>
+</html>
---
+++ Web Lab/boutique shop/eva_product.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
+</head>
+<body>
+
+<h2 style="text-align:center">Product Card</h2>
+
+<div class="card">
+  <img src="pro1.jfif" alt="Designer gown" style="width:100%">
+  <h1>wedding gowns</h1>
+  <p class="price">1459.00</p>
+  <p>beauitful red gown</p>
+  
+  <a href="eva_cart.php">Add to cart</a></p>
+</div>
+
+<div class="card">
+  <img src="pro2.jfif" alt="Designer gown" style="width:100%">
+  <h1>saree gowns</h1>
+  <p class="price">1234.00</p>
+  <p>beauitful green gown</p>
+  <a href="eva_cart.php">Add to cart</a></p>
+</div>
+
+<div class="card">
+  <img src="pro3.jfif" alt="Designer gown" style="width:100%">
+  <h1>saree</h1>
+  <p class="price">1678.00</p>
+  <p>beauitful wedding saree</p>
+  <a href="eva_cart.php">Add to cart</a></p>
+</div>
+
+</body>
+</html>

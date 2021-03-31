0128a97ba12e0a2074db1d06395160828c436cba
anjanaviswanath <77048019+anjanaviswanath@users.noreply.github.com>
2021-03-31 13:13:41 +0530

    delete

--- Python Lab/17-02-2021/CO1/co1-13.py
+++
@@ -1,3 +0,0 @@
-colors=(input('Enter colors separated by commas: ')).split(',')
-print('First color:',colors[0])
-print('Last color:',colors[len(colors)-1])
--- Python Lab/17-02-2021/CO3/Graphics/circle.py
+++
@@ -1,4 +0,0 @@
-def area(r):
-    print('Area of circle with radius',r,'is:','%.2f'%(3.14*r*r),'Sq.units')
-def circumference(r):
-    print('Circumference of circle with radius',r,'is:','%.2f'%(3.14*2*r),'units')
